e9176957b9dbf22502d2ae63d18a29804de8dbd9
lemasani <brianlemasan@gmail.com>
2024-04-15 11:23:33 +0300

    Add react-optimized-image package to package.json and update InternCard component to use react-optimized-image for image rendering

--- src/Pages/Training/Internship.jsx
+++ src/Pages/Training/Internship.jsx
@@ -23,8 +23,17 @@ export default function Internship() {
                 </Col>
             </Row> */}
 
+            <Row>
+              <Col>
+                <h2 className='heading underlined'>Internship</h2>
+                <p>
+                Welcome to the Chair's Internship Program! Each year, we offer six internship opportunities to undergraduate students, providing them with valuable experience at leading NGOs. In 2022-2023, interns from Tanzania worked with Dignity Kwanza, REDESSO, and KINNAPA NGOs. In 2023-2024, interns from Uganda are placed with the International Rescue Committee, Refugee Law Project, and CAFOMI. Additionally, the Chair offers research support to three postgraduate students annually, including postdoctoral, PhD, and Master's students, providing research funds and networking opportunities. In 2022-2023, support was provided to candidates from Kenya, Burundi, and Uganda. Join us in empowering the next generation of scholars and professionals in forced displacement studies!
+                </p>
+              </Col>
+            </Row>
+
+            <Row>
             <div>
-              <h1>Interns</h1>
               {InternData.map((category) => (
                 <div key={category.category}>
                   <h2 className='underlined text-primary mt-3 mb-4 text-center'>{category.category} {category.year} </h2>
@@ -38,7 +47,9 @@ export default function Internship() {
               ))}
             </div>
 
-            <img src="assets/Interns/bahati john.png" alt="" />
+            </Row>
+
+            \
         </Container>
 
         <Footer/>
