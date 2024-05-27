31525ea4f26ee0403619b3039582bb0af0cb99f8
lemasani <brianlemasan@gmail.com>
2024-05-27 11:11:08 +0300

    Update Home page with information on the Research Chair of Forced Displacement

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -11,7 +11,7 @@ export default function Home() {
     <>
     <Carousel/>
     <Container>
-        <Row>
+        {/* <Row>
          <div>
           <h2 className='fs-2 text-center heading'>News</h2>
           <div className="news-contanier">
@@ -24,11 +24,20 @@ export default function Home() {
             ))}
           </div>
          </div>
-        </Row>
+        </Row> */}
 
         <Row className='mt-4'>
-            <h2 className='heading'>Programs under the Reseach chair of forced displacement</h2>
-          <ProgramCardRendered/>
+            <h2 className='heading'>Reseach chair of forced displacement</h2>
+          <p className='text-center text-balance'>
+            UDSM has proposed to institute a research chair that will work across five East African countries-
+            Tanzania, Kenya, Uganda, Rwanda, and Burundi. The research will focus on those that are forcibly
+            displaced due to conflicts and war, those that are forcibly displaced due to climate change and disaster,
+            and those that are displaced due to development or conservation initiatives. This includes a focus on
+            social and legal protection for women and children, and other vulnerable groups in the displaced
+            populations, as well as a focus on host communities, relocated, and resettled populations across these East
+            African countries. The university will use a feminist, decolonial approach, to enable displaced populations
+            to define the research agenda and outreach, making it demand-driven and action oriented.
+          </p>
         </Row>
 
        
